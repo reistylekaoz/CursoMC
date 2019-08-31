d02da562b091e826280e8cbce8e09e0448b0a724
Murilo Vieira <murilovieiracardoso@gmail.com>
2019-08-31 19:18:27 -0300

    Criação do Projeto

---
+++ src/main/java/com/murilovieira/cursomc/CursoMcApplication.java
@@ -0,0 +1,13 @@
+package com.murilovieira.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMcApplication.class, args);
+	}
+
+}
