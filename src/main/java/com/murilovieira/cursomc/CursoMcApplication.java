ac9a99cb2e7536b76998451faf84bab445b7d9fd
Murilo Vieira <murilovieiracardoso@gmail.com>
2019-09-15 15:20:12 -0300

    Criando dados no banco de dados

--- src/main/java/com/murilovieira/cursomc/CursoMcApplication.java
+++ src/main/java/com/murilovieira/cursomc/CursoMcApplication.java
@@ -1,13 +1,32 @@
 package com.murilovieira.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoMcApplication {
+import com.murilovieira.cursomc.domain.Categoria;
+import com.murilovieira.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoMcApplication implements CommandLineRunner{
+
+	@Autowired	         
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
